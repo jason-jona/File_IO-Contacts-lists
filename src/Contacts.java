1fbac729845112a817e8e7f7ace1736b3440f9b3
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-27 16:45:43 -0600

    up to date build

--- src/Contacts.java
+++ src/Contacts.java
@@ -3,24 +3,24 @@ public class Contacts {
         String userInput;
             ContactsMethods.getContacts();
         do {
-            System.out.println("\nContacts Menu Actions\nAdd | Remove | Search | Display |  Quit");
+            System.out.println("\nContacts Menu Actions\n1. Add\n2. Remove\n3. Search\n4. Display \n5. Quit");
             System.out.println("Enter Menu Action: ");
             userInput = ContactsMethods.sc.nextLine();
 
-            if (userInput.matches("(?i)Add")) {
+            if (userInput.matches("(?i)1|Add")) {
                 ContactsMethods.addContact();
-            } else if (userInput.matches("(?i)Remove")) {
+            } else if (userInput.matches("(?i)2|Remove")) {
                 ContactsMethods.removeContacts();
-            } else if (userInput.matches("(?i)Search")) {
+            } else if (userInput.matches("(?i)3|Search")) {
                 ContactsMethods.searchContacts();
-            } else if (userInput.matches("(?i)Display")){
+            } else if (userInput.matches("(?i)4|Display")){
                 ContactsMethods.getContacts();
-            } else if (userInput.matches("(?i)Quit")){
+            } else if (userInput.matches("(?i)5|Quit")){
                 System.out.println("Goodbye");
             } else {
                 System.out.println("Could not find method, please check spelling.");
             }
-        } while (!userInput.matches("(?i)Quit"));
+        } while (!userInput.matches("(?i)5|Quit"));
 
 
     }
